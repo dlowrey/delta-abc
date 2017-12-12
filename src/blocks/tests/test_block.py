e47a6789c76ac235dd4693a610c963654255a000
dlowrey <dane.r.lowrey@gmail.com>
2017-12-12 11:14:18 -0600

    Linted test_block.py

--- src/blocks/tests/test_block.py
+++ src/blocks/tests/test_block.py
@@ -3,23 +3,25 @@ Test the main features of the Block class,
 including creating, adding transactions, mining,
 and verifying the validity of a block.
 """
-from datetime import datetime
-from time import time
-from collections import OrderedDict
 import unittest
-import os
-import json
 from src.blocks.block import Block
 
 
 class TestBlocks(unittest.TestCase):
 
     def setUp(self):
+        """
+        Set up the variables necessary for testing.
+        This includes a mocked block and mocked transaction.
+        """
         # mock a transaction
+        # if anything is changed in this transaction, changes will need
+        # to be made to the block to reflect the correct block_id and
+        # mining_proof fields.
         self.tnx = {
                 'transaction_id': 'faketransactionid',
                 'unlock': {
-                    'public_key':'test', 
+                    'public_key': 'test',
                     'signature': 'testsig'
                     },
                 'input_count': 0,  # dont't care
@@ -27,16 +29,20 @@ class TestBlocks(unittest.TestCase):
                 'output_count': 0,  # don't care
                 'outputs': [],  # don't care
                 }
-                
+
         # mock a block object
-        block_id = '9dccd2b72bf3e49b12bca7d6d5cbc144be5fea17a230f4fefe342639e71a3608'
+        # block_id is unique to this exact block
+        block_id = (
+                '9dccd2b72bf3e49b12bca7d6d5cbc14'
+                '4be5fea17a230f4fefe342639e71a3608'
+                )
         self.block = {
                 'block_id': block_id,
                 'previous_block_id': '',
                 'timestamp': '',
-                'data' : {'faketransactionid': self.tnx},
+                'data': {'faketransactionid': self.tnx},
                 'version': 0,
-                'mining_proof': 944944,
+                'mining_proof': 944944,  # unique to this exact block
                 }
 
         # save the config file
@@ -44,11 +50,14 @@ class TestBlocks(unittest.TestCase):
             self.config_save = f.read()
 
     def tearDown(self):
+        """
+        Undo anything done in the set up that could
+        stick around
+        """
         # reset the config file
         with open('data/node_info.json', 'w') as f:
             f.write(self.config_save)
 
-    
     def test_create_block(self):
         """
         Test creating a new block and adding a single
@@ -89,6 +98,4 @@ class TestBlocks(unittest.TestCase):
         block = Block(**self.block)
         auth = block.verify()
         self.assertFalse(auth)
-
-        
 
